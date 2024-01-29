e7782cb2875b951a64178919ec625bc1ead18658
iftikhar <test1@abc.com>
2024-01-29 09:48:15 +0500

    first commit

---
+++ data.js
@@ -0,0 +1,124 @@
+var data = [{
+      id: 1,
+      name: "John Doe",
+      position: "Branch Manager",
+      department: "Management",
+      salary: 80000,
+      email: "john.doe@example.com"
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      position: "Customer Service Representative",
+      department: "Customer Service",
+      salary: 50000,
+      email: "jane.smith@example.com"
+    },
+    {
+      id: 3,
+      name: "Robert Johnson",
+      position: "Loan Officer",
+      department: "Loans",
+      salary: 70000,
+      email: "robert.johnson@example.com"
+    },
+    {
+      id: 4,
+      name: "Emily White",
+      position: "Accountant",
+      department: "Finance",
+      salary: 55000,
+      email: "emily.white@example.com"
+    },
+    {
+      id: 5,
+      name: "David Brown",
+      position: "IT Specialist",
+      department: "Information Technology",
+      salary: 65000,
+      email: "david.brown@example.com"
+    },
+    {
+      id: 6,
+      name: "Emily White",
+      position: "Accountant",
+      department: "graphic designer",
+      salary: 55000,
+      email: "emily.white@example.com"
+    },
+    {
+      id: 7,
+      name: "Emily White",
+      position: "Accountant",
+      department: "website  designer",
+      salary: 60000,
+      email: "emily.white@example.com"
+    },
+    {
+      id: 8,
+      name: "Emily White",
+      position: "Accountant",
+      department: "website  designer",
+      salary: 60000,
+      email: "emily.white@example.com"
+    },
+    {
+      id: 9,
+      name: "Emily White",
+      position: "Accountant",
+      department: "graphic designer",
+      salary: 55000,
+      email: "emily.white@example.com"
+    },
+    {
+      id: 10,
+      name: "Emily White",
+      position: "Accountant",
+      department: "graphic designer",
+      salary: 55000,
+      email: "emily.white@example.com"
+    },
+    {
+      id: 11,
+      name: "Emily White",
+      position: "Accountant",
+      department: "ui  designer",
+      salary: 85000,
+      email: "emily.white@example.com"
+    },
+    {
+      id: 12,
+      name: "Emily White",
+      position: "Accountant",
+      department: "Finance",
+      salary: 45000,
+      email: "emily.white@example.com"
+    },
+    {
+      id: 13,
+      name: "Emily White",
+      position: "Accountant",
+      department: "graphic designer",
+      salary: 45000,
+      email: "emily.white@example.com"
+    },
+    {
+      id: 14,
+      name: "Emily White",
+      position: "Accountant",
+      department: "graphic designer",
+      salary: 55000,
+      email: "emily.white@example.com"
+    },
+    {
+      id: 15,
+      name: "Emily White",
+      position: "Accountant",
+      department: "website  designer",
+      salary: 60000,
+      email: "emily.white@example.com"
+    },
+  ]
+// You can use this object in your JavaScript code as needed
+
+
